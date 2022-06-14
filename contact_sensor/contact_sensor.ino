83874ca1e34bddaf66e4fecc37c9e999b22c8b4f
Sergio Olivares <sergio.olivares@doofinder.com>
2022-06-14 09:27:01 +0200

    Adding contact sensor example

---
+++ contact_sensor/contact_sensor.ino
@@ -0,0 +1,98 @@
+/*
+ * door_sensor.ino
+ *
+ *
+ *  Based on the TemperatureSensor example by Mixiaoxiao (Wang Bin)
+ *
+ * Note:
+ *
+ * You are recommended to read the Apple's HAP doc before using this library.
+ * https://developer.apple.com/support/homekit-accessory-protocol/
+ *
+ * This HomeKit library is mostly written in C,
+ * you can define your accessory/service/characteristic in a .c file,
+ * since the library provides convenient Macro (C only, CPP can not compile) to do this.
+ * But it is possible to do this in .cpp or .ino (just not so conveniently), do it yourself if you like.
+ * Check out homekit/characteristics.h and use the Macro provided to define your accessory.
+ *
+ * Generally, the Arduino libraries (e.g. sensors, ws2812) are written in cpp,
+ * you can include and use them in a .ino or a .cpp file (but can NOT in .c).
+ * A .ino is a .cpp indeed.
+ *
+ * You can define some variables in a .c file, e.g. int my_value = 1;,
+ * and you can access this variable in a .ino or a .cpp by writing extern "C" int my_value;.
+ *
+ * So, if you want use this HomeKit library and other Arduino Libraries together,
+ * 1. define your HomeKit accessory/service/characteristic in a .c file
+ * 2. in your .ino, include some Arduino Libraries and you can use them normally
+ *                  write extern "C" homekit_characteristic_t xxxx; to access the characteristic defined in your .c file
+ *                  write your logic code (eg. read sensors) and
+ *                  report your data by writing your_characteristic.value.xxxx_value = some_data; homekit_characteristic_notify(..., ...)
+ * done.
+ */
+
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#include "wifi_info.h"
+//include the Arduino library for your real sensor here, e.g. <DHT.h>
+
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+
+const int doorSensor = 13; // (D7) CHANGE 13 TO THE DIGITAL INPUT OF DOOR SENSOR!
+
+void setup() {
+	Serial.begin(115200);
+	wifi_connect(); // in wifi_info.h
+	my_homekit_setup();
+  pinMode(doorSensor, INPUT_PULLUP); 
+}
+
+void loop() {
+	my_homekit_loop();
+	delay(10);
+}
+
+//==============================
+// Homekit setup and loop
+//==============================
+
+// access your homekit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_contact_sensor_state;
+
+static uint32_t next_heap_millis = 0;
+static uint32_t next_report_millis = 0;
+
+void my_homekit_setup() {
+	arduino_homekit_setup(&config);
+}
+
+void my_homekit_loop() {
+	arduino_homekit_loop();
+	const uint32_t t = millis();
+	if (t > next_report_millis) {
+		// report sensor values every 10 seconds
+		next_report_millis = t + 10 * 1000;
+		my_homekit_report();
+	}
+	if (t > next_heap_millis) {
+		// show heap info every 5 seconds
+		next_heap_millis = t + 5 * 1000;
+		LOG_D("Free heap: %d, HomeKit clients: %d",
+				ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+	}
+}
+
+void my_homekit_report() {
+	int door_state = digitalRead(doorSensor);
+
+  if (door_state == HIGH) {
+    cha_contact_sensor_state.value.int_value = 1;
+    LOG_D("Door open");
+  } else {
+    cha_contact_sensor_state.value.int_value = 0;
+    LOG_D("Door closed");
+  }
+	homekit_characteristic_notify(&cha_contact_sensor_state, cha_contact_sensor_state.value);
+}
